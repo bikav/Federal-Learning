961b1a0da6d6bcce31e158d0d46d89218380d1a1
YumuJiu <49934936+YumuJiu-cxx@users.noreply.github.com>
2023-01-14 15:27:09 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,27 @@
+"""
+    构建模型
+"""
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+
+
+def DNN(args, file_name):
+    model = Sequential()
+    model.add(Dense(128, input_dim=args.input_dim))
+    model.add(Activation('relu'))
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dense(32))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(6))
+    model.add(Activation('softmax'))  # 多分类
+
+    # model.summary()  # 模型各层的参数状况
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
